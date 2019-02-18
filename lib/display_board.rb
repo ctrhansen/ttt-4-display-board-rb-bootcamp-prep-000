33ee34f33e3825a6973e76d280cb8b6967158089
Thomas Hansen <ctrhansen@yahoo.com>
2019-02-18 16:11:24 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,9 @@
-def display_board()
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+
+def display_board(board)
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
+end
